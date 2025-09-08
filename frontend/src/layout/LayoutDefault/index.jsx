d1fdf17a509ce997c0426c5da5a8b88398f3aafe
YourGitHubUsername <YourEmail@example.com>
2025-09-08 09:23:49 +0700

    xóa code thừa

--- frontend/src/layout/LayoutDefault/index.jsx
+++ frontend/src/layout/LayoutDefault/index.jsx
@@ -15,35 +15,17 @@ function LayoutDefault() {
                                     Trang chủ
                                 </NavLink>
                             </li>
-                            {/* {token && (<>
-                            <li>
-                                <NavLink to="Topic">
-                                    Chủ đề
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="Answers">
-                                    Các bài đã làm 
-                                </NavLink>
-                            </li>
-                            </>)} */}
+                          
 
                         </ul>
                     </div>
-                    {/* <div className = "layout-default__account">
-                      {token ? (<>
-                        <NavLink to = "/logout" >Đăng xuất</NavLink>
-                      </>) : (<>
-                        <NavLink to = "/login" >dang nhap</NavLink>
-                        <NavLink to = "/register" >dang ky</NavLink>
-                      </>)}
-                    </div> */}
+                  
                 </header>
                 <main className="layout-default__main">
                     <Outlet />
                 </main>
                 <footer className="layout-default__footer">
-                    Quiz 200000000000000000000000
+                    nronlineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
                 </footer>
             </div>
         </>
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,28 +1,8 @@
 import PrivateRoutes from "../components/PrivateRoutes";
 import LayoutDefault from "../layout/LayoutDefault";
 import Home from "../Pages/home/index";
-// import Login from "../Pages/Login";
-// import Register from "../Pages/Register";
-// import Answers from "../Pages/Answers";
-// import Quiz from "../Pages/Quiz";
-// import Result from "../Pages/Result";
-// import Topic from "../Pages/Topic";
-// import Logout from "../Pages/Logout";
-// import CreateTopic from "../Pages/CreateTopic";
 
 export const routes = [
-//   {
-//     path: "/login",
-//     element: <Login />,
-//   },
-//   {
-//     path: "/register",
-//     element: <Register />,
-//   },
-//   {
-//     path: "/logout",
-//     element: <Logout />,
-//   },
   {
     path: "/",
     element: <LayoutDefault />,
@@ -31,31 +11,6 @@ export const routes = [
         index: true,
         element: <Home />,
       },
-    //   {
-    //     path: "createTopic", 
-    //     element: <CreateTopic />,
-    //   },
-    //   {
-    //     element: <PrivateRoutes />,
-    //     children: [
-    //       {
-    //         path: "answers",
-    //         element: <Answers />,
-    //       },
-    //       {
-    //         path: "quiz/:id",
-    //         element: <Quiz />,
-    //       },
-    //       {
-    //         path: "result/:id",
-    //         element: <Result />,
-    //       },
-    //       {
-    //         path: "topic",
-    //         element: <Topic />,
-    //       },
-    //     ],
-    //   },
     ],
   },
 ];
